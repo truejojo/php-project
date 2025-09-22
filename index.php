3e1a2a906a2f3d6d6df6ccd18d6f9f84cab21b99
truejojo <johannesveh@yahoo.de>
2025-09-22 08:05:31 +0200

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<?
+include 'inc/header.php';
+
+// 3:12.24
+// query: ?id=1&catergory=23
+// filter_input
+// INPUT_GET
+// FILTER_VALIDATE_INT
+// INPUT_POST
+?>
+  
+  <h1>Hallo Welt!</h1>
+  <p>Dies ist mein erstes PHP-Projekt.</p>
+
+<?
+include 'inc/footer.php';
+?>
